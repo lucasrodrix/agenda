af801c566cd1933e986d15f2e7b9a69963245f06
Lucas Rodrix <lucasrodrigues1985.lfr@gmail.com>
2021-08-03 09:31:32 -0300

    Correção do TimeZone

--- core/views.py
+++ core/views.py
@@ -4,6 +4,7 @@ from core.models import Evento
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
+from datetime import datetime, timedelta
 
 # Create your views here.
 # def index(request):
@@ -32,7 +33,8 @@ def submit_login(request):
 @login_required(login_url='/login/')
 def lista_eventos(request):
     usuario = request.user
-    evento = Evento.objects.filter(usuario=usuario)
+    data_atual = datetime.now() - timedelta(hours=1)
+    evento = Evento.objects.filter(usuario=usuario, data_evento__gt=data_atual)
     dados = {'eventos':evento}
     return render(request, 'agenda.html', dados)
 
